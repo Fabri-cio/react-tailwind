e4f475c6531b56e2a97aa62435ab1ab4deb1c903
wil <wil.fabri777@gmail.com>
2025-08-31 19:48:40 -0400

    navar un poco arreglado

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,39 +1,35 @@
-import useLogout from '../../hooks/useLogout'
-import { FaBars, FaEllipsisV } from 'react-icons/fa'
-import Dropdown from '../shared/Dropdown'
-import { ActionButton } from '../shared/ActionButton'
+import useLogout from "../../hooks/useLogout";
+import { FaBars, FaEllipsisV } from "react-icons/fa";
+import Dropdown from "../shared/Dropdown";
+import { ActionButton } from "../shared/ActionButton";
 
 const Navbar = ({ toggleSidebar }) => {
-  const logoutUser = useLogout()
+  const logoutUser = useLogout();
 
   return (
-    <nav className="sticky z-5 flex justify-between p-4 text-white bg-gray-500">
-      <ActionButton icon={FaBars} onClick={toggleSidebar} estilos="p-1" />
-      <ActionButton label={'Navbar'} to={'/home'} />
+    <nav className="sticky z-5 flex justify-between p-4 text-white bg-gray-500 shadow-md">
+      <ActionButton
+        icon={FaBars}
+        onClick={toggleSidebar}
+        estilos="p-1 transition-colors duration-200 hover:bg-gray-600 rounded"
+      />
+      <ActionButton
+        label={"Navbar"}
+        to={"/home"}
+        estilos="transition-colors duration-200 hover:text-gray-200"
+      />
 
-      {/* {sidebarToggle && (
-        <div>
-          <Link to="/">Inicio</Link>
-          <Link to="/acerca">Acerca</Link>
-          <Link to="/contacto">Contacto</Link>
-        </div>
-      )} */}
       <div className="flex items-center">
         <Dropdown
           icon={FaEllipsisV}
           options={[
-            {
-              label: 'Mi Perfil',
-            },
-            {
-              label: 'Cerrar Sesión',
-              action: logoutUser,
-            },
+            { label: "Mi Perfil" },
+            { label: "Cerrar Sesión", action: logoutUser },
           ]}
         />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
